c4d0e0399922be00406b27d071a3ea7e121fe683
Melting Face <50713792+Melting-Face@users.noreply.github.com>
2023-04-16 04:16:15 +0900

    Time: 792 ms (14.21%), Space: 19 MB (21.08%) - LeetHub

---
+++ 0997-find-the-town-judge/0997-find-the-town-judge.py
@@ -0,0 +1,14 @@
+from collections import Counter
+
+class Solution:
+    def findJudge(self, n: int, trusts: List[List[int]]) -> int:
+        trust_set = set()
+        trust_dict = {}
+        for trust in trusts:
+            trust_set.add(trust[0])
+            trust_dict[trust[1]] = trust_dict.get(trust[1], 0) + 1
+
+        for i in range(1, n + 1):
+            if i not in trust_set and trust_dict.get(i, 0) == (n - 1):
+                return i
+        return -1
