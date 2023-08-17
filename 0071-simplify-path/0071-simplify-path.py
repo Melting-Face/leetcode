5a1d2c8aa6b6990c80608307b2c82e8d16b48e92
Melting Face <50713792+Melting-Face@users.noreply.github.com>
2023-08-17 20:47:26 +0900

    Time: 48 ms (47.89%), Space: 16.3 MB (64.24%) - LeetHub

---
+++ 0071-simplify-path/0071-simplify-path.py
@@ -0,0 +1,15 @@
+class Solution:
+    def simplifyPath(self, path: str) -> str:
+        arr = []
+        items = [text for text in path.split('/') if text and text != '.']
+        for item in items:
+            if item == '.':
+                continue
+
+            if item == '..':
+                if arr:
+                    arr.pop()
+            else:
+                arr.append(item)
+
+        return f'/{"/".join(arr)}'
