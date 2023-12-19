a09842833e0cbbef0896725307fcf30451583a3f
Melting Face <50713792+Melting-Face@users.noreply.github.com>
2023-12-20 01:55:24 +0900

    Time: 242 ms (83.42%), Space: 19.5 MB (33.42%) - LeetHub

--- 1052-grumpy-bookstore-owner/1052-grumpy-bookstore-owner.py
+++ 1052-grumpy-bookstore-owner/1052-grumpy-bookstore-owner.py
@@ -7,13 +7,18 @@ class Solution:
     ) -> int:
         zipped_customers = zip(customers, grumpy)
         satisfied_customers = sum(
-            customers[:minutes] + [c * bool(not g) for i, (c, g) in enumerate(zipped_customers) if i >= minutes],
+            customers[:minutes]
+            + [
+                c * bool(not g)
+                for i, (c, g) in enumerate(zipped_customers)
+                if i >= minutes
+            ],
         )
         max_satisfied_customers = satisfied_customers
         for i in range(0, len(customers) - minutes):
-            satisfied_customers -= (customers[i] if grumpy[i] else 0)
+            satisfied_customers -= customers[i] if grumpy[i] else 0
             satisfied_customers += customers[i + minutes] if grumpy[i + minutes] else 0
-            max_satisfied_customers= max(
+            max_satisfied_customers = max(
                 max_satisfied_customers,
                 satisfied_customers,
             )
