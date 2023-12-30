2062181554585c97a91e57f804db2a79bff2eef1
Melting Face <50713792+Melting-Face@users.noreply.github.com>
2023-12-30 23:49:34 +0900

    Time: 93 ms (5.12%), Space: 18 MB (7.09%) - LeetHub

--- 0841-keys-and-rooms/0841-keys-and-rooms.py
+++ 0841-keys-and-rooms/0841-keys-and-rooms.py
@@ -1,19 +1,14 @@
-from copy import deepcopy
-
 class Solution:
     def canVisitAllRooms(self, rooms: List[List[int]]) -> bool:
-        remain_rooms = set(rooms[0])
-        total_rooms = deepcopy(remain_rooms)
-        while remain_rooms:
-            temp_room = set()
-            for key in remain_rooms:
-                room_set = set(rooms[key]) - total_rooms
-                if room_set:
-                    temp_room.update(room_set)
-                    total_rooms.update(room_set)
-            remain_rooms = temp_room
-        if 0 in total_rooms:
-            total_rooms.remove(0)
+        total_key_set = set(range(len(rooms)))
+        key_set = set()
+        current_key_set = set(rooms[0])
+        while loop_key_set := (current_key_set - key_set):
+            print(current_key_set)
+            key_set |= current_key_set
+            current_key_set.clear()
+            for key in loop_key_set:
+                current_key_set |= set(rooms[key])
+        key_set |= {0}
 
-        return len(total_rooms) == (len(rooms) - 1)
-        
+        return not bool(total_key_set - key_set)
