67ec4b33c17703a11e6e22875d57dfd047bc7546
Melting Face <50713792+Melting-Face@users.noreply.github.com>
2024-01-31 00:50:42 +0900

    Time: 34 ms (79.49%), Space: 16.5 MB (68.38%) - LeetHub

--- 2579-count-total-number-of-colored-cells/2579-count-total-number-of-colored-cells.py
+++ 2579-count-total-number-of-colored-cells/2579-count-total-number-of-colored-cells.py
@@ -1,6 +1,3 @@
 class Solution:
     def coloredCells(self, n: int) -> int:
-        output = 1
-        for idx in range(0, n):
-            output += (idx * 4)
-        return output
+        return 2 * n * (n - 1) + 1
