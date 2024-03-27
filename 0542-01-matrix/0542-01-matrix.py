341657d2f6766380b2829f9a0e2485713e3090f9
Melting Face <50713792+Melting-Face@users.noreply.github.com>
2024-03-28 02:41:59 +0900

    Time: 8879 ms (5.02%), Space: 19.3 MB (78.58%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.py
+++ 0542-01-matrix/0542-01-matrix.py
@@ -1,33 +1,26 @@
+from collections import deque
+
+
 class Solution:
     def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
         row_length = len(mat)
         col_length = len(mat[0])
-        route_value = 0
-        none_value_count = 999
-        while none_value_count > 0:
-            if route_value == 0:
-                for i, row in enumerate(mat):
-                    for j, value in enumerate(row):
-                        if mat[i][j] != 0:
-                            mat[i][j] = None
-                route_value += 1
-                continue
+        deq = deque()
+        for i, row in enumerate(mat):
+            for j, _ in enumerate(row):
+                if mat[i][j] != 0:
+                    deq.append((i, j, 0))
+                    mat[i][j] = None
 
-            none_value_count = 0
-            for i, row in enumerate(mat):
-                for j, value in enumerate(row):
-                    if value is not None:
-                        continue
-                    if (
-                        (j < col_length - 1 and mat[i][j + 1] == route_value - 1)
-                        or (j > 0 and mat[i][j - 1] == route_value - 1)
-                        or (
-                            i < row_length - 1 and mat[i + 1][j] == route_value - 1
-                        )
-                        or (i > 0 and mat[i - 1][j] == route_value - 1)
-                    ):
-                        mat[i][j] = route_value
-                    else:
-                        none_value_count += 1
-            route_value += 1
+        while deq:
+            i, j, value = deq.popleft()
+            if (
+                (j < col_length - 1 and mat[i][j + 1] == value)
+                or (j > 0 and mat[i][j - 1] == value)
+                or (i < row_length - 1 and mat[i + 1][j] == value)
+                or (i > 0 and mat[i - 1][j] == value)
+            ):
+                mat[i][j] = value + 1
+            else:
+                deq.append((i, j, value + 1))
         return mat
