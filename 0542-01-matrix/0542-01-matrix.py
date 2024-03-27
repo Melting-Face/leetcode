96acc4c040b5f31ac4820a684d326ff09d23ddca
Melting Face <50713792+Melting-Face@users.noreply.github.com>
2024-03-28 01:50:27 +0900

    Time: 9035 ms (5.02%), Space: 18.8 MB (97.12%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.py
+++ 0542-01-matrix/0542-01-matrix.py
@@ -2,31 +2,32 @@ class Solution:
     def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
         row_length = len(mat)
         col_length = len(mat[0])
-        dp = [None for _ in range(row_length * col_length)]
         route_value = 0
-        while None in dp:
+        none_value_count = 999
+        while none_value_count > 0:
             if route_value == 0:
-                for idx, value in enumerate(dp):
-                    row, col = divmod(idx, col_length)
-                    if mat[row][col] == 0:
-                        dp[idx] = 0
+                for i, row in enumerate(mat):
+                    for j, value in enumerate(row):
+                        if mat[i][j] != 0:
+                            mat[i][j] = None
                 route_value += 1
                 continue
 
-            for idx, value in enumerate(dp):
-                if value is not None:
-                    continue
-                row, col = divmod(idx, col_length)
-                if (
-                    (col < col_length - 1 and dp[idx + 1] == route_value - 1)
-                    or (col > 0 and dp[idx - 1] == route_value - 1)
-                    or (
-                        row < row_length - 1 and dp[idx + col_length] == route_value - 1
-                    )
-                    or (row > 0 and dp[idx - col_length] == route_value - 1)
-                ):
-                    dp[idx] = route_value
-                    mat[row][col] = route_value
+            none_value_count = 0
+            for i, row in enumerate(mat):
+                for j, value in enumerate(row):
+                    if value is not None:
+                        continue
+                    if (
+                        (j < col_length - 1 and mat[i][j + 1] == route_value - 1)
+                        or (j > 0 and mat[i][j - 1] == route_value - 1)
+                        or (
+                            i < row_length - 1 and mat[i + 1][j] == route_value - 1
+                        )
+                        or (i > 0 and mat[i - 1][j] == route_value - 1)
+                    ):
+                        mat[i][j] = route_value
+                    else:
+                        none_value_count += 1
             route_value += 1
         return mat
-
