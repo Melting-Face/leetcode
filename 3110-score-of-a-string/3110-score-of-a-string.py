42684e429c512f153a0642fba6f4eb425a0f222a
Melting Face <50713792+Melting-Face@users.noreply.github.com>
2024-08-11 13:00:34 +0900

    Time: 42 ms (21.23%), Space: 16.6 MB (15.08%) - LeetHub

--- 3110-score-of-a-string/3110-score-of-a-string.py
+++ 3110-score-of-a-string/3110-score-of-a-string.py
@@ -1,8 +1,7 @@
 class Solution:
     def scoreOfString(self, s: str) -> int:
-        before_ascii = ord(s[0])
-
         score = 0
+        before_ascii = ord(s[0])
 
         for char in s[1:]:
             current_ascii = ord(char)
