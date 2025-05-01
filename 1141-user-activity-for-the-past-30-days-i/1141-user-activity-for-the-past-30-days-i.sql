739bd1cefa884ac75d881e29133b3c7128d78de6
Melting Face <50713792+Melting-Face@users.noreply.github.com>
2025-05-02 01:34:31 +0900

    Time: 639 ms (43.38%), Space: 0B (100%) - LeetHub

---
+++ 1141-user-activity-for-the-past-30-days-i/1141-user-activity-for-the-past-30-days-i.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+select
+    activity_date as day,
+    count(distinct user_id) as active_users
+from activity
+where activity_date between date('2019-07-27') - interval 29 day
+    and date('2019-07-27')
+group by 1
