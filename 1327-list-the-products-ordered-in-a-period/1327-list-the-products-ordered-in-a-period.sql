d2fa9c950c210a767b6954ad38a2fa53da3ea880
Melting Face <50713792+Melting-Face@users.noreply.github.com>
2025-08-30 11:23:31 +0900

    Time: 729 ms (50.82%), Space: 0B (100%) - LeetHub

---
+++ 1327-list-the-products-ordered-in-a-period/1327-list-the-products-ordered-in-a-period.sql
@@ -0,0 +1,11 @@
+# Write your MySQL query statement below
+select
+    product_name,
+    sum(unit) as unit
+from products
+left join orders
+    using (product_id)
+where year(order_date) = 2020
+  and month(order_date) = 2
+group by 1
+having sum(unit) >= 100
