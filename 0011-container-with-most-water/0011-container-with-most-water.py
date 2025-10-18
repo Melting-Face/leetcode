508e01ec0a26c1877164e6ecf5de45071c11f320
Melting Face <50713792+Melting-Face@users.noreply.github.com>
2025-10-18 21:52:30 +0900

    Time: 101 ms (59.76%), Space: 28.5 MB (24.03%) - LeetHub

---
+++ 0011-container-with-most-water/0011-container-with-most-water.py
@@ -0,0 +1,17 @@
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        left = 0
+        right = len(height) - 1
+        max_water = 0
+
+        while left < right:
+            max_water = max(
+                (right - left) * min(height[left], height[right]),
+                max_water,
+            )
+            if height[left] < height[right]:
+                left += 1
+                continue
+            right -= 1
+        
+        return max_water
